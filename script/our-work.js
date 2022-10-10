2c219261534bb10a3d4d710d999034746cc3871c
overwatch-coder <devbrainy98@gmail.com>
2022-10-10 14:35:19 +0100

    fixed js code

--- script/our-work.js
+++ script/our-work.js
@@ -1,33 +1,67 @@
-// declare variables
-const hamburgerMenu = document.querySelector('#hamburger-menu');
-const hamburgerMenuIcon = document.querySelector('#menu-icon');
-const mobileNavbar = document.querySelector('.navbar-mobile');
-const ourWorks = document.querySelector('#our-works');
-const contactUs= document.querySelector('#contact-us');
-const date = document.querySelector('#date');
+window.addEventListener('DOMContentLoaded', () => {
+    // declare variables
+    const hamburgerMenu = document.querySelector('#hamburger-menu');
+    const hamburgerMenuIcon = document.querySelector('#menu-icon');
+    const mobileNavbar = document.querySelector('.navbar-mobile');
+    const ourWorks = document.getElementById('our-works');
+    const contactUs= document.getElementById('contact-us');
+    const date = document.querySelector('#date');
 
-// adding current year to the footer
-date.innerHTML = new Date().getFullYear();
+    // adding current year to the footer
+    date.innerHTML = new Date().getFullYear();
 
-// adding event listeners
-hamburgerMenu.addEventListener('click', () => {
-    ['bi-list', 'bi-x-lg'].map(menu => {
-        hamburgerMenuIcon.classList.toggle(menu);
+    // adding event listeners
+    hamburgerMenu.addEventListener('click', () => {
+        ['bi-list', 'bi-x-lg'].map(menu => {
+            hamburgerMenuIcon.classList.toggle(menu);
+        });
+
+        if(hamburgerMenuIcon.classList.contains('bi-x-lg')){
+            mobileNavbar.style.transform = 'translateY(0)';
+            mobileNavbar.style.transition = 'transform 1s ease-in-out';
+            contentPushDown();
+        }else{
+            mobileNavbar.style.transform = 'translateY(-100vh)';
+            mobileNavbar.style.transition = 'transform 1s ease-in-out';
+            contentPushUp();
+        }
     });
 
-    if(hamburgerMenuIcon.classList.contains('bi-x-lg')){
-        mobileNavbar.style.transform = 'translateY(0)';
-        mobileNavbar.style.transition = 'transform 1s ease-in-out';
-        [ourWorks, contactUs].map(sectionToGiveMargin => {
-            sectionToGiveMargin.style.margin = '30rem 2rem';
-            sectionToGiveMargin.style.transition = 'margin 1s ease-in-out'
-        })
-    }else{
-        mobileNavbar.style.transform = 'translateY(-100vh)';
-        mobileNavbar.style.transition = 'transform 1s ease-in-out';
-        [ourWorks, contactUs].map(sectionToGiveMargin => {
-            sectionToGiveMargin.style.margin = '10rem 2rem';
-            sectionToGiveMargin.style.transition = 'margin 1s ease-in-out'
-        })
+    // script to push content down or up depending on the direction of navbar
+
+    // get website current pathname
+    const { pathname } = document.location;
+
+    const contentPushDown = () => {
+        switch(pathname){
+            case '/contact-us.html': 
+                navbarPushContentDown(contactUs);
+            case '/our-work.html':
+                navbarPushContentDown(ourWorks);
+            default:
+                navbarPushContentDown(null);
+        }
     }
-});
+
+    const contentPushUp = () => {
+        switch(pathname){
+            case '/contact-us.html': 
+                navbarPushContentUp(contactUs);
+            case '/our-work.html':
+                navbarPushContentUp(ourWorks);
+            default:
+                navbarPushContentUp(null);
+        }
+    }
+
+
+    const navbarPushContentDown = (sectionOne) => {
+        sectionOne.style.margin = '30rem 2rem';
+        sectionOne.style.transition = 'margin 1s ease-in-out';
+    }
+
+    const navbarPushContentUp = (sectionOne) => {
+        sectionOne.style.margin = '10rem 2rem';
+        sectionOne.style.transition = 'margin 1s ease-in-out';
+    }
+})
