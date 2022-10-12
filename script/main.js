fc90047c95f0cd3ae40d4229917ce2e5e2234e4d
brainy <devbrainy98@gmail.com>
2022-10-12 19:17:24 +0100

    modified and merged all codes together into develop branch

--- script/main.js
+++ script/main.js
@@ -5,6 +5,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const mobileNavbar = document.querySelector('.navbar-mobile');
     const ourWorks = document.getElementById('our-works');
     const contactUs= document.getElementById('contact-us');
+    const home= document.getElementById('home');
+    const services= document.getElementById('services');
+    const about= document.getElementById('about');
     const date = document.querySelector('#date');
 
     // adding current year to the footer
@@ -27,8 +30,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    
     // script to push content down or up depending on the direction of navbar
-
     // get website current pathname
     const { pathname } = document.location;
 
@@ -36,10 +39,18 @@ window.addEventListener('DOMContentLoaded', () => {
         switch(pathname){
             case '/contact-us.html': 
                 navbarPushContentDown(contactUs);
+                break;
             case '/our-work.html':
                 navbarPushContentDown(ourWorks);
+                break;
+            case '/index.html':
+                navbarPushContentDown(home);
+                break;
+            case '/services.html':
+                navbarPushContentDown(services);
+                break;
             default:
-                navbarPushContentDown(null);
+                navbarPushContentDown(about);
         }
     }
 
@@ -47,21 +58,39 @@ window.addEventListener('DOMContentLoaded', () => {
         switch(pathname){
             case '/contact-us.html': 
                 navbarPushContentUp(contactUs);
+                break;
             case '/our-work.html':
                 navbarPushContentUp(ourWorks);
+                break;
+            case '/index.html':
+                navbarPushContentUp(home);
+                break;
+            case '/services.html':
+                navbarPushContentUp(services);
+                break;
             default:
-                navbarPushContentUp(null);
+                navbarPushContentUp(about);
         }
     }
 
 
     const navbarPushContentDown = (sectionOne) => {
-        sectionOne.style.margin = '30rem 2rem';
-        sectionOne.style.transition = 'margin 1s ease-in-out';
+        if(pathname == '/services.html'){
+            sectionOne.style.margin = '30em 0 0 0';
+            sectionOne.style.transition = 'margin 1s ease-in-out';
+        }else{
+            sectionOne.style.margin = '30rem 2rem';
+            sectionOne.style.transition = 'margin 1s ease-in-out';
+        }
     }
 
     const navbarPushContentUp = (sectionOne) => {
-        sectionOne.style.margin = '10rem 2rem';
-        sectionOne.style.transition = 'margin 1s ease-in-out';
+        if(pathname == '/services.html'){
+            sectionOne.style.margin = '12em 0 0 0';
+            sectionOne.style.transition = 'margin 1s ease-in-out';
+        }else{
+            sectionOne.style.margin = '10rem 2rem';
+            sectionOne.style.transition = 'margin 1s ease-in-out';
+        }
     }
 })
